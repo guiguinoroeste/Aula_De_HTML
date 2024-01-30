de9b66ab552e5c0deeeb335b8468c992f7df5039
Guilherme José <146457473+guiguinoroeste@users.noreply.github.com>
2024-01-29 22:00:31 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,32 +1,17 @@
-var Peso;
-var Altura;
-IMC = peso / altura * altura
-peso = document.getElemenentById("txtPeso").value;
-addEventListener("click", function(){
-});
-altura = document.getElementById("txtAltura").value;
-addEventListener("click", function(){
-});
-document.getElementById("classificacaoIMC").value;
-addEventListener("click", function(){
-});
-function verificarIMC(IMC) {
-if (imc < 17) {
-    classificacaoIMC('Muito abaixo do peso', 'alert');
-  } else if (imc > 17 && imc <= 18.49) {
-    classificacaoIMC('Abaixo do peso', 'warning');
-  } else if (imc >= 18.5 && imc <= 24.99) {
-    classificacaoIMC('Peso normal', 'sucess');
-  } else if (imc >= 25 && imc <= 29.99) {
-    classificacaoIMC('Acima do peso', 'warning');
-  } else if (imc >= 30 && imc <= 34.99) {
-    classificacaoIMC('Obesidade I', 'alert');
-  } else {
-    classificacaoIMC('Obesidade II', 'danger');
-  }
+function limparDisplay() {
+    document.getElementById('display').value = '';
 }
 
-function classificacaoIMC(msg, type) {
-  
+function apagarUltimo() { 
+    var display = document.getElementById('display');
+    display.value = display.value.slice(0, -1);
 }
 
+function adicionarCaractere(caractere) {  
+    document.getElementById('display').value += caractere;
+}
+
+function calcularResultado() {
+    var display = document.getElementById('display');
+	display.value = eval(display.value);
+}
